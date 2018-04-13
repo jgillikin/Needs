f9edc4aecf5d7e6437ed43fadc05437cb823882c
Jason Gillikin <jgillikin@USIT.LOCAL>
2018-04-13 15:46:41 -0400

    pwawork2

--- server.js
+++ server.js
@@ -10,7 +10,8 @@ app.configure('production', => {
   })
 })
 
-app.use(express.static('www'));
+//app.use(express.static('www'));
+
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), "0.0.0.0", function () {
     console.log('Express server listening on port ' + app.get('port'));
