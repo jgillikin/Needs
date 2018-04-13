d58effe86940e3fc776f9402d7f8f1c60806a649
Jason Gillikin <jgillikin@USIT.LOCAL>
2018-04-13 15:58:31 -0400

    pwaWork3

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express'),
 app = express();
 
-app.configure('production', => {
   app.use((req, res, next) => {
     if (req.header 'x-forwarded-proto' !== 'https')
       res.redirect(`https://${req.header('host')}${req.url}`)
     else
       next()
   })
-})
 
 //app.use(express.static('www'));
 
