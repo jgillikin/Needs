d31398afbcb0562a3a5e1f1eaf091760a2db994b
Jason Gillikin <jgillikin@USIT.LOCAL>
2018-09-20 06:27:02 -0400

    serverFix2

--- server.js
+++ server.js
@@ -1,12 +1,9 @@
-var express = require('express');
-var app     = express();
+var express = require('express'),
+app = express();
 
-app.set('port', (process.env.PORT || 3000));
+app.use(express.static('www'));
 
-//For avoidong Heroku $PORT error
-app.get('/', function(request, response) {
-    var result = 'App is running'
-    response.send(result);
-}).listen(app.get('port'), function() {
-    console.log('App is running, server is listening on port ', app.get('port'));
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), "0.0.0.0", function () {
+    console.log('Express server listening on port ' + app.get('port'));
 });
